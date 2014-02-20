0d2188ef994f307bb07fdbbd3f9d617575408691
Julian Modesto <julianvmodesto@gmail.com>
2014-02-20 18:44:10 -0500

    Rename P_Communicator and P_Parser to PeerCommunicator and PeerParser
    -Just because it's only a few more keystrokes, it's clearer, and there
    are no underscores :-)

--- src/edu/rutgers/cs/cs352/bt/PeerCommunicator.java
+++ src/edu/rutgers/cs/cs352/bt/PeerCommunicator.java
@@ -7,7 +7,7 @@ package edu.rutgers.cs.cs352.bt;
  * @author Julian Modesto
  *
  */
-public class P_Communicator {
+public class PeerCommunicator {
 
 	/**
 	 * 
--- src/edu/rutgers/cs/cs352/bt/PeerParser.java
+++ src/edu/rutgers/cs/cs352/bt/PeerParser.java
@@ -7,6 +7,6 @@ package edu.rutgers.cs.cs352.bt;
  * @author Julian Modesto
  *
  */
-public class P_Parser {
-
+public class PeerParser {
+	
 }
