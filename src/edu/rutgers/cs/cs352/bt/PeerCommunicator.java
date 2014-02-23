03e53406281e2cdbf565b1a2c1d01b1a9f12bbe8
Julian Modesto <julianvmodesto@gmail.com>
2014-02-23 16:54:34 -0500

    Update PeerCommunicator
    - Add logger
    - Begin setting up Socket
    - Change some variable names

--- src/edu/rutgers/cs/cs352/bt/PeerCommunicator.java
+++ src/edu/rutgers/cs/cs352/bt/PeerCommunicator.java
@@ -3,11 +3,18 @@
  */
 package edu.rutgers.cs.cs352.bt;
 
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.logging.Logger;
+
 /**
  * @author Julian Modesto
  *
  */
 public class PeerCommunicator {
+	
+	private final static Logger LOGGER = Logger.getLogger(PeerCommunicator.class.getName());
 	
 	private static final byte[] GROUP = {'G','P','O','1','6'};
 	
@@ -37,8 +44,8 @@ public class PeerCommunicator {
 		handshake[0] = 19;
 		
 		// Add "BitTorrent protocol"
-		byte[] header = {'B','i','t','T','o','r','r','e','n','t',' ','p','r','o','t','o','c','o','l'};
-		System.arraycopy(header, 0, handshake, 1, header.length);
+		byte[] protocol = {'B','i','t','T','o','r','r','e','n','t',' ','p','r','o','t','o','c','o','l'};
+		System.arraycopy(protocol, 0, handshake, 1, protocol.length);
 		
 		// 8 reserved bytes 20-27 are already initialized to 0; skip + omit commented-out code below
 //		// Add 8 reserved bytes which are set to 0
@@ -52,6 +59,32 @@ public class PeerCommunicator {
 		System.arraycopy(peerID, 0, handshake, 48, peerID.length);	
 		
 		return handshake;
+	}
+	
+	/**
+	 * 
+	 */
+	public static void getConnection(String address, int port) {
+		
+		// Check that port number is within standard TCP range i.e. max port number is an unsigned, 16-bit short = 2^16 - 1 = 65535
+		if (port <= 0 | port >= 65535) {
+			LOGGER.warning("Error: port number" + port + "is out of bounds");
+			return;
+		}
+		
+		Socket socket = null;
+		try {
+			socket = new Socket(address, port);
+		} catch (UnknownHostException uhe) {
+			LOGGER.warning("Error: the IP address of the host could not be determined from " + address);
+			uhe.printStackTrace();
+			return;
+		} catch (IOException ioe) {
+			LOGGER.warning("Error: an I/O error occurred");
+			LOGGER.warning(ioe.getMessage());
+			ioe.printStackTrace();
+			return;
+		}
 	}
 	
 	/**
