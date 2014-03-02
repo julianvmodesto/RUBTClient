b9efa25968c0aff6cd1d65747e1e23c94a625c00
brocrastinator <gaurav.b.kumar@gmail.com>
2014-03-02 15:09:41 -0500

    Modified TrackerCommunicator

--- src/edu/rutgers/cs/cs352/bt/TrackerCommunicator.java
+++ src/edu/rutgers/cs/cs352/bt/TrackerCommunicator.java
@@ -3,10 +3,23 @@
  */
 package edu.rutgers.cs.cs352.bt;
 
+import java.util.logging.Logger;
+import java.net.URI;
+import java.net.URISyntaxException;
 /**
- * @author Gaurav
+ * @author Gaurav Kumar
  *
  */
 public class TrackerCommunicator {
-
+	private final static Logger LOGGER = Logger.getLogger(TrackerCommunicator.class.getName());
+	private static final byte[] GROUP = {'G','P','O','1','6'};
+	
+	public static void AnnounceStartToTracker(TorrentInfo torrent_info, ){
+		try {
+			URI uri = new URI(TorrentInfo.);
+		} catch (URISyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
