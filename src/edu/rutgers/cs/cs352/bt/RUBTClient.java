955a2b6cf08a4a249cdb6f2713cdaf7f8d98ec4b
Gaurav Kumar <Gaurav Kumar@The-Dane>
2014-03-02 15:25:32 -0500

    Added public static uploaded and downloaded and left fields to RUBTClient

--- src/edu/rutgers/cs/cs352/bt/RUBTClient.java
+++ src/edu/rutgers/cs/cs352/bt/RUBTClient.java
@@ -7,11 +7,14 @@ import java.io.*;
 
 
 public class RUBTClient {
-	public enum TrackerEvent {
+	public enum Event {
 		STARTED, COMPLETED, STOPPED
 	};
 	private static TorrentInfo torrent_info; //TorrentInfo object used for the program, created in main
-
+	public static int uploaded = 0;
+	public static int downloaded = 0;
+	public static int left = 0;
+	
 	public static void main(String[] args) throws Exception {
 		
 		if (args.length != 2) { // error for incorrect amount of args
