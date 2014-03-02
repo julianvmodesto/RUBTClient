29d923ccdc6592510921b846be028e9e7da46a4e
jrocha1720 <jrocha1720@yahoo.com>
2014-03-02 14:20:15 -0500

    Just changed some stuff around, added comments

--- src/edu/rutgers/cs/cs352/bt/RUBTClient.java
+++ src/edu/rutgers/cs/cs352/bt/RUBTClient.java
@@ -3,31 +3,28 @@ package edu.rutgers.cs.cs352.bt;
 /* @author Jeffrey Rocha */
 
 import java.net.*;
-import java.util.logging.Logger;
 import java.io.*;
-
-import edu.rutgers.cs.cs352.bt.exceptions.BencodingException;
+import java.util.logging.Logger;
 
 public class RUBTClient {
 	
-	private final static Logger LOGGER = 
-			Logger.getLogger(RUBTClient.class.getName());
+	private static TorrentInfo torrent_info; //TorrentInfo object used for the program, created in main
 	
-	private static TorrentInfo torrent_info;
+	private final static Logger LOGGER = Logger.getLogger(RUBTClient.class.getName());
 
 	public static void main(String[] args) throws Exception {
 		
-		if (args.length != 2) {
+		if (args.length != 2) { // error for incorrect amount of args
 			System.err.println("Error: two arguments required");
 			System.exit(1);
 		}
 		
 		String file_name = args[0];
 		File torrent_file = new File(file_name);
-		byte[] torrent_bytes = getFileInBytes(torrent_file, file_name);
+		byte[] torrent_bytes = getFileInBytes(torrent_file, file_name); //calls getFileInBytes to change the torrent file into a byte array
 		
 		torrent_info = new TorrentInfo(torrent_bytes);
-		
+				
 		RUBTClient client = new RUBTClient();
 		client.getRequest();
 	}
@@ -44,11 +41,15 @@ public class RUBTClient {
 		{
 			file_stream = new FileInputStream(torrent_file);
 			
-			if (!torrent_file.exists()) //Check to see if the file exists
-				System.err.println("The file \"" + file_name + "\" does not exist. Please make sure you have the correct path to the file.");
+			if (!torrent_file.exists()){ //Check to see if the file exists
+				System.err.println("The file \"" + file_name + "\" does not exist. Find the correct path to the file.");
+				return null;
+			}
 			
-			if (!torrent_file.canRead()) //Check to see if the file is readable
-				System.err.println("Cannot read from \"" + file_name + "\". Please make sure the file permissions are set correctly.");
+			if (!torrent_file.canRead()){ //Check to see if the file is readable
+				System.err.println("Cannot read from \"" + file_name + "\". File permissions must be set correctly.");
+				return null;
+			}
 			
 			int file_offset = 0;
 			int bytes_read = 0;
@@ -57,17 +58,17 @@ public class RUBTClient {
 			while (file_offset < torrent_bytes.length && (bytes_read = file_stream.read(torrent_bytes, file_offset, torrent_bytes.length - file_offset)) >= 0)
 				file_offset += bytes_read;
 
-			if (file_offset < torrent_bytes.length)
+			if (file_offset < torrent_bytes.length) { //In case the whole file could not be read
 				throw new IOException("Could not completely read file \"" + torrent_file.getName() + "\".");
-		
-			file_stream.close();
-			
+			}
+			file_stream.close();	
 		}
-		catch (FileNotFoundException fnfe)
+		catch (FileNotFoundException fnfe) //file was not found
 		{
-			System.err.println("The file \"" + file_name + "\" does not exist. Please make sure you have the correct path to the file.");
+			System.err.println("The file \"" + file_name + "\" does not exist. Find the correct path to the file.");
+			return null;
 		}
-		catch (IOException ioe)
+		catch (IOException ioe) //I/O error
 		{
 			System.err.println("There was an I/O error when reading \"" + file_name + "\".");
 			System.err.println(ioe.getMessage());
@@ -78,14 +79,15 @@ public class RUBTClient {
 	/* Sends HTTP Get Request to 
 	 * the tracker
 	 */
-	private void getRequest() throws Exception{
+	public void getRequest() throws Exception{
 		
-		String urlName = torrent_info.announce_url.toString();
-		URL obj = new URL(urlName);
-		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+		String urlName = torrent_info.announce_url.toString(); //makes the URL of the torrentInfo object into a string
+		URL url = new URL(urlName); // new URL object made from the string announce_url
+		HttpURLConnection con = (HttpURLConnection) url.openConnection(); //open an HTTP connection
  
 		con.setRequestMethod("GET");
  
+		//response code used to find if connection was success or failure (and reason for failure)
 		int responseCode = con.getResponseCode();
 		System.out.println("\nSending 'GET' request to URL : " + urlName);
 		System.out.println("Response Code : " + responseCode);
